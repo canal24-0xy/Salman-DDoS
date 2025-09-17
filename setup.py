65e4b5f8fd617a0335986f3b5c4f8686f8fe16bf
canal24-0xy <canalkun24@gmail.com>
2025-09-17 11:07:08 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ c = input(">>>: ")
 if c == "0":
     os.system("apt install python")
     os.system("apt install python2")
-    os.system("apt-get install python3")
+    os.system("pip install display_header")
     os.system("git pull")
    
 
 elif c == "1":
     os.system("apt install python")
     os.system("apt install python2")
-    os.system("apt-get install python3")
+    os.system("pip3 install display_header")
     os.system("git pull")
    
    
